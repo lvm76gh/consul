9f00a62f83aa7baa060c224758507253b0c32ebd
lvm76gh <lmarks@gmail.com>
2017-02-19 13:34:26 -0800

    Modified chown order

--- terraform/shared/scripts/install-nomad.sh
+++ terraform/shared/scripts/install-nomad.sh
@@ -35,8 +35,8 @@ EOF
   sudo chown root:root /etc/nomad.d/server.hcl
   sudo chmod 0644 /etc/nomad.d/server.hcl
 
-  sudo chown root:root /tmp/debian_nomad_upstart.conf
   sudo mv /tmp/debian_nomad.upstart.conf /etc/init/nomad.conf
+  sudo chown root:root /tmp/debian_nomad_upstart.conf
   sudo chmod 0644 /etc/init/nomad.conf
   sudo mv /tmp/nomad_flags /etc/service/nomad
   sudo chmod 0644 /etc/service/nomad
