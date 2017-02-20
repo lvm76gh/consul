78915735e9b505aad057053da37f4b89aaa01a1b
lvm76gh <lmarks@gmail.com>
2017-02-20 14:14:43 -0800

    d

--- terraform/shared/scripts/service-vault.sh
+++ terraform/shared/scripts/service-vault.sh
@@ -9,5 +9,4 @@ if [ -x "$(command -v systemctl)" ]; then
 else 
   echo "using upstart"
   sudo start vault
-# curl -X POST -H "X-Vault-Token:9fbfbe48-8a5a-b50e-dbfc-0b7189f546b0" -d '{"type":"approle"}' http://172.31.28.194:8200/v1/sys/auth/approle
 fi
