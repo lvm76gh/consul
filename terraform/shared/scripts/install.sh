ab9e57b52bbf35b8ef60477e2034ed88c7258e44
lvm76gh <lmarks@gmail.com>
2017-02-21 08:09:00 -0800

    d

--- terraform/shared/scripts/install.sh
+++ terraform/shared/scripts/install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-#echo "Installing dependencies..."
+echo "Installing dependencies..."
 #if [ -x "$(command -v apt-get)" ]; then
 #  sudo apt-get update -y
-#  sudo apt-get install -y unzip
+   sudo apt-get install -y unzip
 #else
 #  sudo yum update -y
 #  sudo yum install -y unzip wget
